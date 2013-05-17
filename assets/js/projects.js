466a54375bf7428812aaee678a9cb5b407a167ae
Ｄａｖｉｄ Ｈｉｇｇｉｎｓ <manicstrike@gmail.com>
2013-05-17 23:57:38 +0100

    The one true commit

---
+++ assets/js/projects.js
@@ -0,0 +1,103 @@
+var projData = [
+
+{dir: "entifier",
+desc:
+'You have HTML code you want to place in a &lt;PRE&gt; tag element. You want it entified, so it plays nice with your PRE tag. This does that.'},
+
+{dir: "dataurl",
+desc:
+'A homage to the wonderful tool Bocoup have released. You need to convert raw bytecode to base64 encoded URI data. This does that'},
+
+{dir: "packer",
+desc:
+'You have JavaScript. You need to pack it to a really small size. This does that. Based on Ben Alman\'s (@cowboy\'s) packer script.'},
+
+{dir: "textarea",
+desc:
+'Sometimes you just need a textarea, right? Sometimes I work on locked down machines, where tools like Textmate, or if on Windows, notepad; aren\'t available. Also, because Firefox has the spellchecker turned on by default, I can check my spelling really quickly.'},
+
+{dir: "beautifier",
+desc:
+'Formats and prettifies Ugly JavaScript.'},
+
+{dir: "bookmarklet",
+desc:
+'You have JavaScript. You need a bookmarklet. This does that.'},
+
+{dir: "notice",
+desc:
+'Your site breaks, fails, and generally just doesn\'t work in IE6? Re-direct your users to this tongue in cheek notifier page. Yeah, I\'m a modern browser fanboy. If this offends you, don\'t complain. IE6 is officialy dead, anyways. And the latest IE is apparently lightyears ahead of previous versions.'},
+
+{dir: "jjencoder",
+desc:
+'The wonderful tool at UTF8.jp reincarnated and open sourced here, on iwantaneff.in. You have JS you need obfuscated beyond recognition. This does that.'},
+
+{dir: "escaper",
+desc:
+'You have a Unicode symbol you want to use in your .CSS file. You need to sanitize it for ASCII based text editors. This does that. Only works on character ranges inside the BMP.'},
+
+
+{dir: "star",
+desc:
+'Quick access to the most copied-and-pasted Unicode symbol, evar. ★ '},
+
+
+{dir: "smiley",
+desc:
+'Quick access to the most copied-and-pasted Unicode symbol, evar. ☺ '},
+
+{dir: "repo",
+desc:
+'The Handpicked jQuery Plugins Repository is a carefully selected roundup of some of the best jQuery Plugins ever released. '},
+
+{dir: "favicon",
+desc:
+'Sometimes you just want a favicon without the fuss. This tool is for those who want to stifle favicon 404 errors on the backend and frontend.'},
+
+{dir: "cloudapp",
+desc:
+'What this program does is generate a random 4 letter alphanumberic file ID, and appends it to a cloudapp URL. It then opens ten browser tabs / windows so you can see what Cloudapp item exists there. '},
+
+{dir: "glitcher",
+desc:
+'You have text. You need to fuck it up. This does that.'},
+
+{dir: "opener",
+desc:
+'You have a bunch of cyberlocker links. You need to open them all at once. This does that.'},
+
+{dir: "asciimation",
+desc:
+'A script that animates a very long ASCII rendering of a star.'},
+
+{dir: "pacman",
+desc:
+'Sometimes you\'re just bored!'},
+
+{dir: "404",
+desc:
+'This demo / experiment is a fake Firefox 404 error page with a dead "Link" (From Zelda) on the page. I often re-direct people to this, hence why it was added.'},
+
+{dir: "hotpink",
+desc:
+'Hotpink is the greatest colour ever.'},
+
+{dir: "stream",
+desc:
+'SweetCron powered Twitter stream of all the main players in the frontend scene. Not your average reading session.'},
+
+{dir: "shared",
+desc:
+'My whole hard-drive online for all to see. Not your average collection of files. A lot going on.'},
+
+{dir: "chromeless",
+desc:
+'Let\'s make the internet a more beautiful place '},
+
+{dir: "twitter",
+desc:
+'This Tweetdump is a proof of concept experiment. It shows you don\'t have to let Twitter swallow up all your Tweets once they reach the incremental 3200 API limit. If you\'re not backing up your Tweets, and then deleting them, you really should be doing so.'}
+
+
+
+];
--- readme.md
+++ readme.md
@@ -8,54 +8,6 @@ Website: [http://iwantaneff.in/toolset/][2]
 
 You are browsing the Github home of **Effin Toolset - A suite of tools for web developers**
 
-[![Foo](http://api.flattr.com/button/flattr-badge-large.png)](https://flattr.com/thing/470390)
-
-Devtools
-- [http//iwantaneff.in/entifier/](http//iwantaneff.in/entifier/)
-- [http//iwantaneff.in/dataurl/](http//iwantaneff.in/dataurl/)
-- [http//iwantaneff.in/packer/](http//iwantaneff.in/packer/)
-- [http//iwantaneff.in/textarea/](http//iwantaneff.in/textarea/)
-- [http//iwantaneff.in/beautifier/](http//iwantaneff.in/beautifier/)
-- [http//iwantaneff.in/bookmarklet/](http//iwantaneff.in/bookmarklet/)
-- [http//iwantaneff.in/notice/](http//iwantaneff.in/notice/)
-- [http//iwantaneff.in/jjencoder/](http//iwantaneff.in/jjencoder/)
-
-Unicode
-- [http//iwantaneff.in/.txt](http//iwantaneff.in/.txt)
-- [http//iwantaneff.in/escaper/](http//iwantaneff.in/escaper/)
-- [http//iwantaneff.in/star/](http//iwantaneff.in/star/)
-- [http//iwantaneff.in/flower/](http//iwantaneff.in/flower/)
-- [http//iwantaneff.in/smiley/](http//iwantaneff.in/smiley/)
-
-Resources
-- [http//iwantaneff.in/custom.webkit.scrollbar.css](http//iwantaneff.in/custom.webkit.scrollbar.css)
-- [http//iwantaneff.in/favicon/](http//iwantaneff.in/favicon/)
-- [http//iwantaneff.in/facebook.ping/](http//iwantaneff.in/facebook.ping/)aaa
-- [http//iwantaneff.in/cursor.css](http//iwantaneff.in/cursor.css)
-
-Misc Tools
-- [http//iwantaneff.in/cloudapp/](http//iwantaneff.in/cloudapp/)
-- [http//iwantaneff.in/glitcher/](http//iwantaneff.in/glitcher/)
-- [http//iwantaneff.in/opener/](http//iwantaneff.in/opener/)
-- [http//iwantaneff.in/asciimation/](http//iwantaneff.in/asciimation/)
-- [http//iwantaneff.in/pacman/](http//iwantaneff.in/pacman/)
-
-Projects:
-- [http//iwantaneff.in/stream/](http//iwantaneff.in/stream/)
-- [http//iwantaneff.in/linkdump/](http//iwantaneff.in/linkdump/)
-- [http//iwantaneff.in/repo/](http//iwantaneff.in/repo/)
-- [http//iwantaneff.in/feed/](http//iwantaneff.in/feed/)
-- [http//iwantaneff.in/filedump/](http//iwantaneff.in/filedump/)
-- [http//iwantaneff.in/gallery/](http//iwantaneff.in/gallery/)
-- [http//iwantaneff.in/chromeless/](http//iwantaneff.in/chromeless/)
-- [http//iwantaneff.in/twitter/](http//iwantaneff.in/twitter/)
-- [undefined](undefined)
-
-
-
-----------
-
-
   [1]: http://higg.im/
   [2]: http://iwantaneff.in/toolset/
   
